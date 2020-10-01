61fa736fc4b7a968598c2afd7d3fc612239662da
vio7enc3 <70806255+vio7enc3@users.noreply.github.com>
2020-10-01 19:56:57 +0500

    add task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined) return 0;
+  if (array.length !== 0){
+    var minValue = array[0];
+    for (var i = 0; i < array.length; i++) {
+      if (minValue > array[i]) minValue = array[i];
+    }
+    return minValue;
+    
+  } else return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined) return 0;
+  if (array.length === 0){
+    return 0;
+  }
+  else {
+    var maxValue = array[0];
+    for (var i = 0; i < array.length; i++){
+      if (maxValue < array[i]) maxValue = array[i];
+    }
+    return maxValue;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined) return 0;
+  var sum = 0;
+  if (array.length === 0) return 0;
+  else {
+    var avgValue = array[0];
+    for (var i = 0; i < array.length; i++){
+      sum += array[i];
+      avgValue = sum / array.length;
+    }
+    return avgValue;
+  }
 }
